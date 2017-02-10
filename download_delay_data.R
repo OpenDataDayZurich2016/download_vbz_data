a04e1a194d4c73ed35d332a32f387ce77d5b5ca5
HeidiSeibold <heidi.seibold@uzh.ch>
2017-02-10 15:04:12 +0100

    download works now

--- download_delay_data.R
+++ download_delay_data.R
@@ -3,13 +3,10 @@ source("get_data_links.R")
 
 
 links_years <- list(links_2015, links_2016, links_2017)
-data_dirs <- paste("data", 2015:2017, sep = "_")
 
-## create directories, if not yet existent
-lapply(data_dirs, function(ddir) {
-  if(!dir.exists(ddir))
-    dir.create(ddir)
-})
+## create directory, if not yet existent
+if(!dir.exists("data/"))
+  dir.create("data/")
 
 
 get_available_timeslots <- function(links = links_2016) {
@@ -23,21 +20,25 @@ get_available_timeslots <- function(links = links_2016) {
   ## reformat in POSIXct
   data.frame(from = strptime(timestmp, format = "%Y%m%d"),
              to = strptime(substring(timestmp, first = 9), format = "%Y%m%d"),
-             link = lks)
+             link = as.character(lks), stringsAsFactors = FALSE)
 }
 
 timeslots <- ldply(links_years, get_available_timeslots)
+timeslots$filename <- sapply(strsplit(timeslots$link, split = "/download/"), function(x) x[2])
 
 
 
 download_delay_data <- function(date, timeslots = timeslots) {
   
   thislink <- date >= timeslots$from & date <= timeslots$to
-  timeslots[thislink, ]
+  slot <- timeslots[thislink, ]
+  download.file(slot$link, paste0("data/", slot$filename))
   
 }
 
-download_delay_data(date = as.POSIXct())
+
+## pick a date and download the data set with the given date, e.g.
+download_delay_data(date = as.POSIXct("2016-02-18"), timeslots = timeslots)
 
 
 

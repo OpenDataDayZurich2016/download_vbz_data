cffb2be681cf84e14243e7be62935793a474e8c3
Christoph Molnar <christoph.molnar@gmail.com>
2017-02-11 14:01:05 +0100

    add script to download passenger counts for 2014

---
+++ download_passenger_counts.R
@@ -0,0 +1,8 @@
+## Downloads the passenger counts data 
+
+
+zip_url = 'https://data.stadt-zuerich.ch/dataset/vbz_fahrgastzahlen_ogd/resource/1d94f238-ac45-4829-8248-1c30b7a85623/download/fahrgastzahlen2014.zip'
+
+zip_filename = './data/passenger_counts.zip'
+download.file(zip_url, destfile=zip_filename)
+unzip(zip_filename, exdir='./data/passenger_counts/')
